d579aa79ff98dd113092b804d51677dff782c352
mehmet <mehmet@localhost>
2015-11-09 15:02:22 +0200

    Uygulama Yapısı ve Veri Modeli Oluşturuldu.

---
+++ src/main/java/tr/gov/teias/egitim/adresdefteri/Adres.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tr.gov.teias.egitim.adresdefteri;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author mehmet
+ */
+@Entity
+public class Adres implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @Column(name = "TCKN", nullable = false, length = 11,  unique = true)
+    @NotNull @Size(min = 11, max = 11)
+    private String tckn;
+    
+    @Column(name = "AD")
+    @NotNull  
+    private String ad;
+    
+    
+    @Column(name = "SOYAD")
+    @NotNull
+    private String soyad;
+    
+    @Column(name = "MOBIL")
+    private String mobil;
+    @Column(name = "TEL")
+    private String tel;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "ADRES")
+    private String adres;
+       
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTckn() {
+        return tckn;
+    }
+
+    public void setTckn(String tckn) {
+        this.tckn = tckn;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getMobil() {
+        return mobil;
+    }
+
+    public void setMobil(String mobil) {
+        this.mobil = mobil;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Adres)) {
+            return false;
+        }
+        Adres other = (Adres) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "tr.gov.teias.egitim.adresdefteri.Adres[ id=" + id + " ]";
+    }
+    
+}
